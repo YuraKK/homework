f25e243ae396e5c6ed632d574d34b27f622b9f3c
YuraKK <yuri.kachur@gmail.com>
2022-02-02 17:44:16 +0200

    js homework

---
+++ Js/Homework1/Homework.js
@@ -0,0 +1,61 @@
+let word = ('Hello');
+let second = ('owu');
+let third = ('com');
+let four = ('ua');
+console.log(word);
+console.log(second);
+document.write(third);
+console.log(four);
+
+let masiv = [1, 10, -999, 123, 3.14, 2.7, 16];
+let one = 1;
+let ten = 10;
+let nine = -999;
+let onetwo = 123;
+let Pi = 3.14;
+let twopoint = 2.7;
+let six = 16;
+
+console.log(one,ten,nine,onetwo,Pi,twopoint,six);
+
+let someM = ('newworld');
+console.log(someM);
+
+one = 11;
+ten = 100;
+nine= 9;
+onetwo = 12;
+Pi = 41.3;
+twopoint = 2.1;
+six = 6;
+console.log(one,ten,nine,onetwo,Pi,twopoint,six);
+
+let name = 'Yura';
+let fathername = 'Genrihovich';
+let ages = 21;
+
+let names = prompt('Say your name', name);
+
+let fatherNames = prompt('Say your fathers name',fathername);
+
+let age = prompt('How old are you', ages);
+
+alert(`Hello ${names} ${fatherNames} you are ${age}`);
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a,b,c);
+
+console.log(5 > 6);
+console.log(5 == 6);
+console.log(5 > 6);
+console.log(5 >= 6);
+
+console.log(10 == 10);
+console.log(10 >= 10);
+console.log(10 != 10);
+console.log(10 > 10);
+
+console.log(10 < 10);
+console.log(123 != '123');
+console.log(123 == '123');
